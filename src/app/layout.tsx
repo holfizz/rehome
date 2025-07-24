fb09cd2ab8701bfe0805e3e7ea3df2505a5e8af8
holfizz <gorlach7v@gmail.com>
2025-07-24 21:09:44 +0900

    Удален файл изображения "case9_ph2.webp". Добавлен счетчик Яндекс.Метрики в компонент layout.tsx. Обновлены названия, описания и характеристики нескольких проектов в файле projects.ts для улучшения контента и SEO.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -127,6 +127,32 @@ export default function RootLayout({
 					href='https://fonts.gstatic.com'
 					crossOrigin='anonymous'
 				/>
+
+				{/* Yandex.Metrika counter */}
+				<script
+					type='text/javascript'
+					dangerouslySetInnerHTML={{
+						__html: `
+							(function(m,e,t,r,i,k,a){
+								m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+								m[i].l=1*new Date();
+								for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+								k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+							})(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=103474269', 'ym');
+
+							ym(103474269, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+						`,
+					}}
+				/>
+				<noscript>
+					<div>
+						<img
+							src='https://mc.yandex.ru/watch/103474269'
+							style={{ position: 'absolute', left: '-9999px' }}
+							alt=''
+						/>
+					</div>
+				</noscript>
 			</head>
 			<body className={inter.className}>{children}</body>
 		</html>
